81ffdbb9bb155dcb7fee3861784391b9b8193312
Vladimir Starkov <iamstarkov@gmail.com>
2016-05-17 11:14:51 +0200

    unused files showcase

--- src/cli.js
+++ src/cli.js
@@ -12,6 +12,7 @@ const log = console.log; // eslint-disable-line
 const err = console.error; // eslint-disable-line
 
 // log('yo');
+const relativeToCwd = _ => path.relative(process.cwd(), _);
 
 Promise.all([
   loadJsonFile('./package.json'),
@@ -26,8 +27,10 @@ Promise.all([
 ]) => {
   const existingFiles = _existingFiles.map(_ => path.join(process.cwd(), _));
   const usedFiles = R.union(prodFiles, testFiles);
-  // log(usedFiles);
-  // log(existingFiles);
+  const unusedFiles = R.difference(existingFiles, usedFiles);
+
   log(`\`${pkgName}\` uses ${usedFiles.length} files out of ${existingFiles.length} existing`);
+  log('Unused Files:');
+  log( unusedFiles.map(relativeToCwd).map(_ => '  ✗ ' + _).join('\n') );
 })
 .catch(err);
