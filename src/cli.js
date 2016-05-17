f3062b8a89b3f1e050f380adb71ccfa2fdb31fd9
Vladimir Starkov <iamstarkov@gmail.com>
2016-05-17 10:17:35 +0200

    mvp yolo

--- src/cli.js
+++ src/cli.js
@@ -1,51 +1,29 @@
 /* eslint-disable no-underscore-dangle */
 import R from 'ramda';
 import Promise from 'pinkie-promise';
-import { allFiles, testFiles } from './fs';
-// import { pkgEsDeps, pkgEsDepsTest } from './pkg-es-deps';
-import kit from 'es-dep-kit';
-// import loadJsonFile from 'load-json-file';
+import { allFiles } from './fs';
+import { pkgEsDeps, pkgEsDepsTest } from './pkg-es-deps';
+// import kit from 'es-dep-kit';
+import loadJsonFile from 'load-json-file';
 // import isBuiltinModule from 'is-builtin-module';
 
-const log = console.l eslint-disable-line
-const thr = msg => { throw msg; };
+const log = console.log; // eslint-disable-line
+const err = console.error; // eslint-disable-line
+
+// log('yo');
 
 Promise.all([
+  loadJsonFile('./package.json'),
+  pkgEsDeps('./package.json'),
+  pkgEsDepsTest('./'),
   allFiles('./'),
 ]).then(([
-  allFilesList,
+  { name: pkgName },
+  prodFiles,
+  testFiles,
+  existingFiles,
 ]) => {
-  // console.log(pkg);
-  /*
-  const actualUsedFiles = deps
-    .filter(R.either(kit.isEntry, kit.isRequestedLocalFile))
-    .filter(kit.isResolved)
-    .map(kit._resolved);
-  */
-
-  log('Files');
-  log('  All ', allFilesList.length);
-  log('  Used', actualUsedFiles.length);
-
-  /*
-  const allProdDeps = R.pipe(R.prop('dependencies'), R.keys)(pkg);
-  const allDeps = R.pipe(R.props(['d', 'v']), R.mergeAll, R.keys);
-  const getModules = R.pipe(
-    R.filter(kit.isRequestedPackage),
-    R.map(kit._requested),
-    R.map(R.pipe(R.split('/'), R.head)),
-    R.reject(isBuiltinModule)
-  );
-
-  const actualUsedModules = getModules(deps);
-  const actualTestModules = getModules(depsTestList);
-
-  log('Modules');
-  log('  Declared', allProdDeps.length);
-  log('  Actual  ', actualUsedModules.length);
-  log('Dev Modules');
-  log('  Declared', allDeps.length);
-  log('  Actual  ', actualUsedModules.length);
-  */
+  const usedFiles = R.union(prodFiles, testFiles);
+  log(`\`${pkgName}\` uses ${usedFiles.length} files out of ${existingFiles.length} existing`);
 })
-.catch(thr);
+.catch(err);
--- src/pkg-es-deps.js
+++ src/pkg-es-deps.js
@@ -2,16 +2,22 @@
 
 import R from 'ramda';
 import _deep from 'es-deps-deep';
-import loadJson from 'load-json-file';
-import entry from 'pkg-entry';
+// import loadJson from 'load-json-file';
+// import entry from 'pkg-entry';
 import Promise from 'pinkie-promise';
 import contract from 'neat-contract';
-import resolveCwd from 'resolve-cwd';
 import kit from 'es-dep-kit';
+import pkgEntryAndBinResolved from 'pkg-entry-and-bin-resolved';
 import p from 'path';
 import { testFiles } from './fs';
 
 const deep = R.curryN(2, _deep);
+
+/**
+ * debug mode
+ */
+const log = console.log;
+const id = R.identity;
 
 // toPromise :: a -> Promise a
 const toPromise = Promise.resolve.bind(Promise);
@@ -19,21 +25,16 @@ const toPromise = Promise.resolve.bind(Promise);
 // all :: [Promise] -> Promise
 const all = Promise.all.bind(Promise);
 
-// getEntry :: String -> Promise String
-const getEntry = R.pipeP(loadJson, entry);
-
-// resolvePath :: Promise... -> String
-const resolvePath = R.pipeP(R.unapply(all), R.apply(p.resolve));
 
 // pkgEsDeps :: String -> Promise [Object]
 function pkgEsDeps(pkg) {
   return R.pipeP(toPromise,
     contract('pkg', String),
-    resolveCwd,
-    contract('pkg', String),
-    R.converge(resolvePath, [p.dirname, getEntry]),
-    R.of,
-    deep(R.__, { excludeFn: kit.isThirdParty })
+    pkgEntryAndBinResolved,
+    deep(R.__, { excludeFn: kit.isThirdParty }),
+    R.filter(kit.isResolved),
+    R.map(kit._resolved),
+    id
   )(pkg);
 }
 
@@ -42,9 +43,15 @@ function pkgEsDepsTest(path) {
   return R.pipeP(toPromise,
     contract('path', String),
     testFiles,
-    R.map(_ => p.join(path, _)),
-    R.map(_ => './' + _),
-    deep(R.__, { excludeFn: kit.isThirdParty })
+    R.map(R.pipe(
+      _ => p.join(path, _),
+      _ => `./${_}`
+    )),
+    deep(R.__, { excludeFn: kit.isThirdParty }),
+    R.filter(kit.isResolved),
+    R.filter(R.either(kit.isEntry, kit.isRequestedLocalFile)),
+    R.map(kit._resolved),
+    id
   )(path);
 }
 
