cdcfcd38ae0ca2cd510b4236f1a9554be01d7d52
Vladimir Starkov <iamstarkov@gmail.com>
2016-05-18 14:44:36 +0200

    finish init

--- src/cli.js
+++ src/cli.js
@@ -9,8 +9,9 @@ import loadJsonFile from 'load-json-file';
 // import isBuiltinModule from 'is-builtin-module';
 import kit from 'es-dep-kit';
 
-const prodModules = R.pipe(R.prop('dependencies'), R.values);
-const devModules = R.pipe(R.prop('devDependencies'), R.values);
+const _declProdModules = R.pipe(R.prop('dependencies'), R.keys);
+const _declDevModules = R.pipe(R.prop('devDependencies'), R.keys);
+
 const preCwd = _ => path.join(process.cwd(), _);
 
 const log = console.log; // eslint-disable-line
@@ -22,6 +23,10 @@ const _files = R.pipe(
   R.map(kit._resolved)
 );
 
+const _modules = R.pipe(
+  R.filter(kit.isRequestedPackage),
+  R.map(kit._requested)
+);
 
 // log('yo');
 const relativeToCwd = _ => path.relative(process.cwd(), _);
@@ -45,21 +50,34 @@ Promise.all([
   const existingFiles = _existingFiles.map(preCwd);
   const unusedFiles = R.difference(existingFiles, usedFiles);
 
-  const declaredProdModules = prodModules(pkg);
-  const declaredDevModules = devModules(pkg);
+  const prodModules = _modules(prodDeps);
+  const devModules = _modules(devDeps);
+  const declaredProdModules = _declProdModules(pkg);
+  const declaredDevModules = _declDevModules(pkg);
+  const unusedProdModules = R.difference(declaredProdModules, prodModules);
+  const unusedDevModules = R.difference(declaredDevModules, devModules);
 
   return {
     pkgName,
     usedFiles,
     existingFiles,
     unusedFiles,
+    prodModules,
+    devModules,
     declaredProdModules,
     declaredDevModules,
+    unusedProdModules,
+    unusedDevModules,
   };
-}).then(({ pkgName, usedFiles, existingFiles, unusedFiles, declaredProdModules }) => {
-  log(`\`${pkgName}\` uses ${usedFiles.length} files out of ${existingFiles.length} existing`);
-  log(`\`${pkgName}\` uses ${'meow'} prod modules out of ${declaredProdModules.length} declared`);
+}).then(_ => {
+  log(`\`${_.pkgName}\` uses ${_.usedFiles.length} files out of ${_.existingFiles.length} existing`);
+  log(`\`${_.pkgName}\` uses ${_.prodModules.length} prod modules out of ${_.declaredProdModules.length} declared`);
+  log(`\`${_.pkgName}\` uses ${_.devModules.length} dev modules out of ${_.declaredDevModules.length} declared`);
   log('Unused Files:');
-  log(unusedFiles.map(relativeToCwd).map(_ => `  ✗ ${_}`).join('\n'));
+  log(_.unusedFiles.map(relativeToCwd).map(_ => `  ✗ ${_}`).join('\n'));
+  log('Unused Prod Modules:');
+  log(_.unusedProdModules.map(_ => `  ✗ ${_}`).join('\n'));
+  log('Unused Dev Modules:');
+  log(_.unusedDevModules.map(_ => `  ✗ ${_}`).join('\n'));
 })
 .catch(err);
