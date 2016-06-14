830f518ad4a6537445c7b1e64d922f7e51ce2272
Vladimir Starkov <iamstarkov@gmail.com>
2016-06-14 19:09:32 +0200

    write tests for deplint

--- test/deplint.js
+++ test/deplint.js
@@ -2,8 +2,46 @@ import test from 'ava';
 import deplint from '../src/deplint';
 import path from 'path';
 
-test.skip('sorted-object', async t => {
+const preFixtures = fixturesPath => _ => path.join(process.cwd(), fixturesPath, _);
+
+test('sorted-object', async t => {
+  const preSortedObject = preFixtures('./fixtures/deplint/sorted-object/');
   const _ = await deplint(path.resolve(__dirname, './fixtures/deplint/sorted-object/'));
-  console.log(_);
-  t.is(1, 1);
+  const expected = {
+    files: {
+      existing: [
+        './lib/sorted-object.js',
+        './test/tests.js',
+        './extra.js',
+      ].map(preSortedObject),
+      used: [
+        './lib/sorted-object.js',
+        './test/tests.js',
+      ].map(preSortedObject),
+      unused: [
+        './extra.js',
+      ].map(preSortedObject),
+    },
+    modules: {
+      declared: [
+        'es-dep-kit',
+        'es-dep-unit',
+        'es-deps-deep',
+        'globby',
+        'jshint',
+        'tape',
+      ],
+      used: [
+        'tape',
+      ],
+      unused: [
+        'es-dep-kit',
+        'es-dep-unit',
+        'es-deps-deep',
+        'globby',
+      ],
+    },
+  };
+
+  t.deepEqual(_, expected);
 });
