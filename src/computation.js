3e6296d2a1387cdc969a8a5e5b781af0e7d7c0a9
Vladimir Starkov <iamstarkov@gmail.com>
2016-06-27 13:14:34 +0200

    fix computation

--- src/computation.js
+++ src/computation.js
@@ -1,12 +1,9 @@
 /* eslint-disable no-underscore-dangle */
-import path from 'path';
 import R from 'ramda';
 import kit from 'es-dep-kit';
 
 const _declProdModules = R.pipe(R.prop('dependencies'), R.keys);
 const _declDevModules = R.pipe(R.prop('devDependencies'), R.keys);
-
-const preCwd = _ => path.join(process.cwd(), _);
 
 const log = console.log; // eslint-disable-line
 
@@ -21,8 +18,6 @@ const _modules = R.pipe(
   R.map(kit._requested)
 );
 
-const relativeToCwd = _ => path.relative(process.cwd(), _);
-
 function computation([pkg, prodDeps, devDeps, _existingFiles]) {
   const pkgName = pkg.name;
   const prodFiles = _files(prodDeps);
@@ -36,7 +31,7 @@ function computation([pkg, prodDeps, devDeps, _existingFiles]) {
     used: R.union(prodFiles, devFiles),
     existing: _existingFiles,
   };
-  files.unused = R.difference(files.existing, files.used).map(relativeToCwd);
+  files.unused = R.difference(files.existing, files.used);
 
   const modules = {
     used: R.union(prodModules, devModules),
